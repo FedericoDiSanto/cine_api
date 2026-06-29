eb8a2e173e2910c617ab4ea2bb0023a15960dcfe
Federico Di Santo <federicojavier.disanto@usal.edu.ar>
2026-06-29 00:51:09 -0300

    cambio el permiso de comentario

--- comentarios/api/permissions.py
+++ comentarios/api/permissions.py
@@ -6,8 +6,9 @@ class IsAdminOrReadAndCreateOnly(BasePermission):
         if request.method == "GET" or request.method == "POST":
             return True
         else:
-            comentario_id = view.kwargs["pk"]
-            comentario_bd = Comentario.objects.get(pk = comentario_id)
+            if 'pk' not in view.kwargs:
+                comentario_id = view.kwargs["pk"]
+                comentario_bd = Comentario.objects.get(pk = comentario_id)
 
             if request.user.id == comentario_bd.usuario.id:
                 return True
