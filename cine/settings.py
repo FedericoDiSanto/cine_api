7281665636e78296141189a8ecb0aadd2bb8b05c
Federico Di Santo <federicojavier.disanto@usal.edu.ar>
2026-06-29 01:16:19 -0300

    cambio el permiso de comentario, e instalo el whitenoise y modifico el middleware para ver los archivos estaticos en docs

--- cine/settings.py
+++ cine/settings.py
@@ -55,7 +55,8 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-]
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+    ]
 
 ROOT_URLCONF = "cine.urls"
 
